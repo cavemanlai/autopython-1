76b8c18bd611db09c72579cef4ce0385b90b13a9
daize.song <daize.song@fairlinkcentury.com>
2016-11-14 18:09:57 +0800

    autopython

--- test.py
+++ test.py
@@ -1,9 +1,34 @@
 
+tableData = [
+    ['apples', 'oranges', 'cherries', 'banana'],
+    ['Alice', 'Bob', 'Carol', 'David'],
+    ['dogs', 'cats', 'moose', 'goose'],
+    ]
 
-def comma_code(text):
-    spam[-1] = 'and ' + spam[-1]
-    comma_list = ', '.join(spam)
-    return(comma_list)
+def colWidth(tableData):
+    colWidths = [0] * len(tableData)
+    for col in range(len(tableData)):
+        for item in tableData[col]:
+            if len(item) > colWidths[col]:
+                colWidths[col] = len(item)
+    return colWidths
 
-spam = ['apples', 'bananas', 'tofu', 'cats']
-print(comma_code(spam))
+def pivot(tableData):
+    columns = []
+    for item in range(len(tableData[0])):
+        for col in range(len(tableData)):
+            try:
+                columns[item].append(tableData[col][item])
+            except:
+                columns.append([tableData[col][item]])
+    return columns
+
+def print_table(tableData):
+    length = colWidth(tableData)
+    for row in pivot(tableData):
+        line = ''
+        for item in range(len(row)):
+            line += row[item].rjust(length[item] + 1)
+        print(line)
+
+print_table(tableData)
