83df8134fd0d73a0175e970cf4b75eb4c4eb495b
daize.song <daize.song@fairlinkcentury.com>
2016-11-24 18:16:20 +0800

    autopython

---
+++ act9/backupToZip.py
@@ -0,0 +1,7 @@
+#! python3
+# backupToZip.py - copies an entire folder and its contents into a ZIP file whose filename increments
+
+import os
+import zipfile
+
+os.chdir('C:\\Users\\daize\\Desktop\\pythontest\\autopython\\act9')
--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 import re
+import os
+import send2trash
 
-text = '''
-42
-1,234
-6,368,745
-12,34,567
-1234
-1,234,567,789
-'''
-
-mo = re.compile(r'^\d{1,3}(?:,\d{3})*$',re.M)
-
-print(mo.findall(text))
+send2trash.send2trash()
+os.remove()
