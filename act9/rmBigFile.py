1f4603bd53cac276c149292f91a617ffd07992cd
sdz0716 <sdz0716@163.com>
2016-11-27 23:56:51 +0800

    autopython-test

--- act9/rmBigFile.py
+++ act9/rmBigFile.py
@@ -3,3 +3,12 @@
 
 import os
 
+def rmbigfile(folder):
+    print(os.path.abspath(folder))
+    for folders, subs, files in os.walk(os.path.abspath(folder)):
+        for file in files:
+            if os.path.getsize(folders + '\\' + file) > 104857600:
+                print(folders + '\\' + file + ':' + str(os.path.getsize(folders + '\\' + file)))
+
+
+rmbigfile('D:\\game\\The Sims 4')
