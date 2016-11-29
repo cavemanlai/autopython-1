93f3d6d400e1ebe1fe032ea6009bac3e421e7415
daize.song <daize.song@fairlinkcentury.com>
2016-11-29 19:15:11 +0800

    autopython

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
 import re
 import os
 import urllib.request, urllib.parse
+import http.cookiejar
 
+# url = 'http://192.168.10.61/zabbix/chart2.php?graphid=897&period=3600&stime=20161128130648&updateProfile=1&profileIdx=web.screens&profileIdx2=897&sid=254050934e847cac&width=1776&screenid=&curtime=1480319746979'
 url = 'http://192.168.10.61/zabbix/index.php'
-# url = 'http://192.168.10.61/zabbix/tr_status.php?filter_set=1&groupid=2&hostid=0&show_triggers=1&sid=254050934e847cac'
-value = {'username': 'Admin', 'password': 'fairlink'}
+value = {'Username': 'Admin', 'Password': 'fairlink'}
 data = urllib.parse.urlencode(value).encode('utf-8')
-req = urllib.request.Request(url, data)
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+# req = urllib.request.Request(url, data)
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0')]
+req = opener.open(url, data)
+# req = opener.open('http://192.168.10.61/zabbix/chart2.php?graphid=897&period=3600&width=1776&stime=20161129000000')
+
 # print(req)
+
 # response = urllib.request.urlopen(req).read()
-response = urllib.request.urlopen(req)
+# response = urllib.request.urlopen(req)
 # print(response)
-# print(response.read())
+print(req.read())
 # print(response.getcode())
-print(response.getallheaders())
 # urllib.request.build_opener()
 # http://192.168.10.61/zabbix/chart2.php?graphid=897&period=3600&stime=20161128130648&updateProfile=1&profileIdx=web.screens&profileIdx2=897&sid=254050934e847cac&width=1776&screenid=&curtime=1480319746979
